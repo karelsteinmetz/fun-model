ae8d4c4a948ca4e73e7292b029b379a17c5fc1e7
k.steinmetz <karel.steinmetz@gmail.com>
2015-10-02 15:54:39 +0200

    Improved build infrastructure.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,27 @@
 var ts = require('gulp-typescript');
 var jasmine = require('gulp-jasmine');
 
+var distDir = 'dist'
+var testDir = 'tests'
+
 gulp.task('default', ['tsCompilation', 'runTests']);
 
 gulp.task('tsCompilation', function () {
     var tsProject = ts.createProject('tsconfig.json');
     return tsProject.src()
         .pipe(ts(tsProject)).js
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(testDir));
 });
 
 gulp.task('runTests', ['tsCompilation'], function () {
-    return gulp.src('dist/spec/**/*.spec.js')
+    return gulp.src(testDir + '/spec/**/*.spec.js')
         .pipe(jasmine({
             verbose: true,
             includeStackTrace: true
         }));
+});
+
+gulp.task('srcTsMove', function () {
+    return gulp.src('src/**/*.ts')
+        .pipe(gulp.dest(distDir + '/src'));
 });
