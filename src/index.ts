7027690c2a460d8a860966b1f032c1fa3f5ff3e5
k.steinmetz <karel.steinmetz@gmail.com>
2015-10-09 09:07:46 +0200

    Added helpers to index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './store';
 export * from './actionFactory';
+export * from './helpers';
 
+export * from './helpers';
 import { IState } from './store';
 import { bootstrap as storeBootstrap } from './store';
 import { bootstrap as actionFactoryBootstrap } from './actionFactory';
