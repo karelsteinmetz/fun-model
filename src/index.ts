f9191a534fdae26c7459abadb5df988b3a527793
keeema <rut.tomas@hotmail.com>
2015-11-07 14:57:53 +0100

    Added missing export of debug

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as d from './debug';
 export * from './store';
 export * from './actionFactory';
 export * from './helpers';
+export { debugCallbackType } from './debug';
 
 export let bootstrap = (defaultState: s.IState, renderCallback: () => void, debugCallback: d.debugCallbackType = undefined, subStateSeparator: string = '.') => {
     debugCallback && d.bootstrap((m, p) => debugCallback(`fun-model -> ${m}`, p));
