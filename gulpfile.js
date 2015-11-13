4f1a257f752b7bd9d8faeb28c5fb887133108d22
k.steinmetz <karel.steinmetz@gmail.com>
2015-11-13 08:03:57 +0100

    Started 0.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var ts = require('gulp-typescript');
 var jasmine = require('gulp-jasmine');
 var exec = require('child_process').exec
 
-var distDir = 'dist'
+var buildDir = 'build'
 var testDir = 'tests'
 
-gulp.task('default', ['runTests', 'srcTsCopy', 'tsCompilation']);
+gulp.task('default', ['runTests', 'tsCompilation']);
 
 gulp.task('tsCompilation', function (cb) {
-    exec('tsc --p ./', function (err, stdout, stderr) {
+    exec('tsc --p ./ --outDir ' + buildDir, function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
@@ -32,27 +32,30 @@ gulp.task('testTsCompilation', function (cb) {
   });
 });
 
-gulp.task('srcTsCopy', function () {
-    return gulp.src('src/**/*.ts')
-        .pipe(gulp.dest(distDir + '/src'));
-});
-
 gulp.task('jamsineCoreCopy', function () {
     return gulp.src('node_modules/jasmine-core/lib/jasmine-core/**/*')
-        .pipe(gulp.dest(distDir + '/jasmine-core'));
-});
-
-gulp.task('jamsineCoreCopy', function () {
-    return gulp.src('node_modules/jasmine-core/lib/jasmine-core/**/*')
-        .pipe(gulp.dest(distDir + '/jasmine-core'));
+        .pipe(gulp.dest(buildDir + '/jasmine-core'));
 });
 
 gulp.task('testResourcesCopy', ['systemjsCopy'], function () {
     return gulp.src('resources/test/**/*')
-        .pipe(gulp.dest(distDir));
+        .pipe(gulp.dest(buildDir));
 });
 
 gulp.task('systemjsCopy', function () {
     return gulp.src('node_modules/systemjs/dist/system.js')
+        .pipe(gulp.dest(buildDir));
+});
+
+// Dist
+var distDir = 'dist'
+
+gulp.task('dist', ['srcTsCopy'], function () {
+    return gulp.src(buildDir + '/src/**/*')
+        .pipe(gulp.dest(distDir));
+});
+
+gulp.task('srcTsCopy', function () {
+    return gulp.src('src/**/*.ts')
         .pipe(gulp.dest(distDir));
 });
