f59ebdf1cd15405d4017d37e61d6a23432f5d535
k.steinmetz <karel.steinmetz@gmail.com>
2015-12-17 09:11:09 +0100

    Removed optional return in shallow copy.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-export function shallowCopy<T>(source: T, callback: (target: T) => T | void = (t: T) => { return t; }): T {
+export function shallowCopy<T>(source: T, callback: (target: T) => void = (t: T) => { }): T {
     let target = <T>{};
     for (var property in source)
         if (source.hasOwnProperty(property))
