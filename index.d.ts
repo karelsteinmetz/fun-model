096934a1b5bde7cbb8f92ad72a90b57a8ce356f4
k.steinmetz <karel.steinmetz@gmail.com>
2016-12-05 22:31:51 +0100

    Used Bobril-build.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 import * as s from './src/store';
+import * as d from './src/debug';
 export * from './src/store';
 export * from './src/actionFactory';
 export * from './src/helpers';
 export { debugCallbackType } from './src/debug';
-export declare let bootstrap: (defaultState: s.IState, renderCallback: () => void, debugCallback?: (message: string, params?: any) => void, subStateSeparator?: string) => void;
+export declare let bootstrap: (defaultState: s.IState, renderCallback: () => void, debugCallback?: d.debugCallbackType, subStateSeparator?: string) => void;
--- src/debug.d.ts
+++ src/debug.d.ts
@@ -1,3 +1,3 @@
 export declare type debugCallbackType = (message: string, params?: any) => void;
-export declare let bootstrap: (debugCallback: (message: string, params?: any) => void) => void;
+export declare let bootstrap: (debugCallback: debugCallbackType) => void;
 export declare function log(message: string, params?: any): void;
